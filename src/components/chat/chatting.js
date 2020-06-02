31332c070eb0a1cc493d9438140ca29a72001358
Muhammad Awais <chdryawais.ca@gmail.com>
2020-06-02 20:45:35 +0500

    docs files, images in chat done

--- src/components/chat/chatting.js
+++ src/components/chat/chatting.js
@@ -17,6 +17,7 @@ export default class Chatting extends Component {
       img: "",
       uploading: false,
       text: "",
+      file: ""
     }
   }
 
@@ -41,13 +42,30 @@ export default class Chatting extends Component {
     
   }
 
-  // Image select
-  imageSelect = (e) => {
+  // File select
+  fileSelect = (e) => {
     let fullPath = e.target.files[0];
-    this.setState({
-      img: fullPath,
-      text: `Image(${fullPath.name})`,
-    });
+    console.log(fullPath)
+    if(fullPath != null) {
+      const name = fullPath.name;
+      const type = fullPath.type;
+      const lastDot = name.lastIndexOf('.');
+
+      const fileName = name.substring(0, lastDot);
+      const ext = type.split('/');
+      console.log('name => ', fileName, ' => ',' extension', ext[0])
+      if(ext[0] == 'image'){
+        this.setState({
+          img: fullPath,
+          text: `Image(${fileName})`,
+        });
+      } else {
+        this.setState({
+          file: fullPath,
+          text: `File(${fileName})`,
+        });
+      }
+    }
   };
   // Uploading image to firebase
   uploadImage = async (image) => {
@@ -56,6 +74,18 @@ export default class Chatting extends Component {
       throw error;
     });
     const url = await imageRef.getDownloadURL().catch((error) => {
+      throw error;
+    });
+    return url;
+  };
+
+  // Uploading file to firebase
+  uploadFile = async (file) => {
+    const fileRef = storage.ref(`/chat-file/${file.name}`);
+    await fileRef.put(file).catch((error) => {
+      throw error;
+    });
+    const url = await fileRef.getDownloadURL().catch((error) => {
       throw error;
     });
     return url;
@@ -76,10 +106,10 @@ export default class Chatting extends Component {
   };
 
   onSend(messages = []) {
-    let { chatID, img, designer } = this.state;
+    let { chatID, img, designer, file } = this.state;
     // let client = JSON.parse(localStorage.getItem("logged"));
     // console.log('IM', fileName, 'PA', img, messages)
-    if (img.length == 0) {
+    if (img.length === 0 && file.length === 0) {
       messages.forEach((item) => {
         const message = {
           id: item.id,
@@ -103,6 +133,33 @@ export default class Chatting extends Component {
       this.setState((previousState) => ({
         messages: GiftedChat.append(previousState.messages, messages),
       }));
+    } else if (file.length !== 0 && img.length === 0 ) {
+      this.setState({ uploading: true });
+      console.log("uploading file......")
+      this.uploadFile(file).then((url) => {
+        messages.forEach((item) => {
+          const message = {
+            id: item.id,
+            text: url,
+            createdAt: new Date().getTime(),
+            user: {
+              id: designer.id,
+              name: designer.name,
+              avatar: designer.image == null ? "https://cdn.pixabay.com/photo/2016/04/01/10/11/avatar-1299805_960_720.png" :designer.image,
+            },
+            type: "text",
+            image: "",
+          };
+          this.sendMessage(message, chatID).then((msg) => {});
+          
+          this.setState((previousState) => ({
+            messages: GiftedChat.append(previousState.messages, messages),
+            file: "",
+            text: "",
+          }));
+        });
+        this.setState({ uploading: false });
+      });
     } else {
       this.setState({ uploading: true });
       this.uploadImage(img).then((url) => {
@@ -159,18 +216,18 @@ export default class Chatting extends Component {
   renderComposer = (props) => {
     return (
       <div className="composer">
-        <div className="image-upload">
+        <div className="file-upload">
           <label for="file-input">
             <i
-              class="fa fa-image"
+              class="fa fa-paperclip"
               style={{ fontSize: "25px", color: "#FF9944", paddingLeft: 10 }}
             />
           </label>
           <input
             type="file"
             id="file-input"
-            accept="image/*"
-            onChange={this.imageSelect}
+            accept="image/*,.doc,.docx,.pdf,application/msword,application/pdf,text/plain"
+            onChange={this.fileSelect}
           />
         </div>
         <Composer {...props} />
@@ -178,8 +235,13 @@ export default class Chatting extends Component {
     );
   };
 
+  handleUrlPress(url, matchIndex /*: number*/) {
+    window.open(url, "_blank")
+  }
+
   render() {
     let {loading} = this.state
+ 
     return (
       <>
         {loading ?
@@ -207,6 +269,9 @@ export default class Chatting extends Component {
                   onSend={(messages) => this.onSend(messages)}
                   renderSend={this.renderSend}
                   renderComposer={this.renderComposer}
+                  parsePatterns={(linkStyle) => [
+                    { type: 'url', style: linkStyle, onPress: this.handleUrlPress },
+                  ]}
                   user={{
                     id: this.state.designer.id,
                   }}
